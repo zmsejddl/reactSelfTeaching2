be8251e0b2d3b2024d11cf21dff993c77a4662c6
Jung Young Hun <hujung0268@gamil.com>
2024-06-20 22:13:44 +0900

    new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/.gitignore" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/README.md" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/package-lock.json" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/package.json" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/public/favicon.ico" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/public/index.html" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/public/logo192.png" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/public/logo512.png" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/public/manifest.json" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/public/robots.txt" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/App.css" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/App.js" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/App.test.js" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/chapter_10/AttendanceBook.jsx" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/index.css" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/index.js" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/logo.svg" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/reportWebVitals.js" 	new file:   "Chapter10 \353\246\254\354\212\244\355\212\270\354\231\200 \355\202\244/my-app/src/setupTests.js" 	new file:   "Chapter11 \355\217\274/my-app/.gitignore" 	new file:   "Chapter11 \355\217\274/my-app/README.md" 	new file:   "Chapter11 \355\217\274/my-app/package-lock.json" 	new file:   "Chapter11 \355\217\274/my-app/package.json" 	new file:   "Chapter11 \355\217\274/my-app/public/favicon.ico" 	new file:   "Chapter11 \355\217\274/my-app/public/index.html" 	new file:   "Chapter11 \355\217\274/my-app/public/logo192.png" 	new file:   "Chapter11 \355\217\274/my-app/public/logo512.png" 	new file:   "Chapter11 \355\217\274/my-app/public/manifest.json" 	new file:   "Chapter11 \355\217\274/my-app/public/robots.txt" 	new file:   "Chapter11 \355\217\274/my-app/src/App.css" 	new file:   "Chapter11 \355\217\274/my-app/src/App.js" 	new file:   "Chapter11 \355\217\274/my-app/src/App.test.js" 	new file:   "Chapter11 \355\217\274/my-app/src/index.css" 	new file:   "Chapter11 \355\217\274/my-app/src/index.js" 	new file:   "Chapter11 \355\217\274/my-app/src/logo.svg" 	new file:   "Chapter11 \355\217\274/my-app/src/reportWebVitals.js" 	new file:   "Chapter11 \355\217\274/my-app/src/setupTests.js"

---
+++ Chapter10 리스트와 키/my-app/src/chapter_10/AttendanceBook.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const students = [
+    {
+        id: 1,
+        name: "Inje",
+    },
+    {
+        id: 2,
+        name: "Steve",
+    },
+    {
+        id: 3,
+        name: "Bill",
+    },
+    {
+        id: 4,
+        name: "Jeff",
+    },
+];
+
+function AttendanceBook(props) {
+    return (
+        <ul>
+            {/* id를 키값으로 사용 */}
+            {students.map((student) => {
+                return <li key={student.id}>{student.name}</li>;
+            })}
+            {/* 포맷팅 된 문자열을 키값으로 사용
+            {students.map((student, index) => {
+                return <li key={`student-id-${student.id}`}>{student.name}</li>;
+            })} */}
+            {/* 배열의 인덱스를 키값으로 사용
+            {students.map((student, index) => {
+                return <li key={index}>{student.name}</li>;
+            })} */}
+        </ul>
+    )
+}
+
+export default AttendanceBook;
