47de0701b51d6e6c77272cfcd67c6b4224617d22
Jung Young Hun <hujung0268@gamil.com>
2024-06-21 22:04:28 +0900

    new file:   "Chapter11 \355\217\274/my-app/src/chapter_11/SignUp.jsx" 	modified:   "Chapter11 \355\217\274/my-app/src/index.js" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/.gitignore" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/README.md" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/package-lock.json" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/package.json" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/public/favicon.ico" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/public/index.html" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/public/logo192.png" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/public/logo512.png" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/public/manifest.json" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/public/robots.txt" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/App.css" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/App.js" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/App.test.js" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/chapter_12/Calculator.jsx" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/chapter_12/TemperatureInput.jsx" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/index.css" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/index.js" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/logo.svg" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/reportWebVitals.js" 	new file:   "Chapter12 State \353\201\214\354\226\264\354\230\254\353\246\254\352\270\260/my-app/src/setupTests.js"

--- Chapter12 State 끌어올리기/my-app/src/index.js
+++ Chapter12 State 끌어올리기/my-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>
 );
 
